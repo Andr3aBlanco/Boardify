aa56d5c303f9436da98a0d292f9cbdf5435c0976
Andrea Blanco <andr3ablanco@gmail.com>
2023-07-01 20:25:07 -0700

    Stripe Service DTOs and Controller - FrontEnd not created yet

--- src/main/java/com/boardify/boardify/BoardifyApplication.java
+++ src/main/java/com/boardify/boardify/BoardifyApplication.java
@@ -3,7 +3,9 @@ package com.boardify.boardify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:application.properties")
 @SpringBootApplication
 public class BoardifyApplication extends SpringBootServletInitializer {
 
