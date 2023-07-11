1126930cfac93c9092ad897879987e44f2dafeb5
Grizanter <ichrisortilla@hotmail.com>
2023-07-11 02:02:23 -0700

    -Used simpler login and registration tutorial to complete authentication -fixed error that prevented go-premium page from displaying -added dummy login profiles for testing purposes: user:admin@admin.com password:#admin
    user:basic@basic.com
    password:#basic
    
    user:premium@premium.com
    password:#premium

--- src/main/java/com/boardify/boardify/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/boardify/boardify/configuration/WebSecurityConfiguration.java
@@ -1,48 +1,55 @@
 package com.boardify.boardify.configuration;
 
-import com.boardify.boardify.repository.UserRepository;
-import com.boardify.boardify.service.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration {
 
-    @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository) {
-        return new UserDetailsServiceImpl(userRepository);
-    }
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizeHttpRequests) ->
-                        authorizeHttpRequests
-                        .requestMatchers("/").permitAll() // Allow access to home page for all
-                                .requestMatchers("/leaderboard").permitAll() // Allow access to home page for all
-                                .requestMatchers("/join-tournament").permitAll() // Allow access to home page for all
-                                .requestMatchers("/create-tournament").permitAll() // Allow access to home page for all
-                        .requestMatchers("/admin/**").hasRole("ADMIN") // Require role "ADMIN" for /admin/**
-                        .requestMatchers("/regular/**").hasRole("USER") // Require role "USER" for /regular/**
-                        .requestMatchers("/premium/**").hasRole("PREMIUM_USER") // Require role "PREMIUM_USER" for /premium/**
-                        .anyRequest().authenticated() // Require authentication for all other URLs
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers("/register/**").permitAll()
+                                .requestMatchers("/index").permitAll()
+                                .requestMatchers("/users").hasRole("ADMIN")
+                                .requestMatchers("/**").permitAll()
+                ).formLogin(
+                        form -> form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/users")
+                                .permitAll()
+                ).logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .permitAll()
                 );
-
         return http.build();
-
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**", "/css/**");
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 }
--- src/main/java/com/boardify/boardify/entities/Role.java
+++ src/main/java/com/boardify/boardify/entities/Role.java
@@ -3,19 +3,24 @@ import jakarta.persistence.*;
 
 import lombok.*;
 
+import java.util.List;
 
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
-@Table(name = "role")
-public class Role {
-
+@Table(name="roles")
+public class Role
+{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long roleId;
+    private Long id;
+
+    @Column(nullable=false, unique=true)
     private String name;
+
+    @ManyToMany(mappedBy="roles")
+    private List<User> users;
 }
