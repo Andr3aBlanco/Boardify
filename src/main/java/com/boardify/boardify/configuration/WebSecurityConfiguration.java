89407b5263e7510c63abad625057a9124c918d35
fikretengindurmaz <fikretengindurmaz@gmail.com>
2023-07-13 17:04:42 -0700

    when someone logged in , now login button turns to logout button, also greeting message added

--- src/main/java/com/boardify/boardify/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/boardify/boardify/configuration/WebSecurityConfiguration.java
@@ -54,8 +54,8 @@ public class WebSecurityConfiguration {
                 )
                 .exceptionHandling(
                         exceptionHandling -> exceptionHandling
-                                .accessDeniedPage("/"))
-        ;
+                                .accessDeniedPage("/"));
+
 
         return http.build();
     }
